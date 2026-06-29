1934405d71e650fdf08decd29bd2f93b4a757285
saraabdelfattahali159 <saraabdelfattahali159@gmail.com>
2026-06-29 23:36:47 +0300

    remove from the header par K mac button

--- src/admin/components/Header.jsx
+++ src/admin/components/Header.jsx
@@ -77,9 +77,6 @@ export const Header = ({ sidebarToggle, setSidebarToggle, darkMode, setDarkMode 
                   </svg>
                 </span>
                 <input type="text" id="admin-search-input" placeholder="Search products, orders..." className="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pr-14 pl-12 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[430px] dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30" />
-                <button type="button" id="admin-search-btn" className="absolute top-1/2 right-2.5 inline-flex -translate-y-1/2 items-center gap-0.5 rounded-lg border border-gray-200 bg-gray-50 px-[7px] py-[4.5px] text-xs -tracking-[0.2px] text-gray-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400">
-                  <span>⌘</span><span>K</span>
-                </button>
               </div>
             </form>
           </div>
